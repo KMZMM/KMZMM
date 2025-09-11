107cbd770f875d0cce56a4c88298fc1e7bc2be08
KMZMM <126046193+KMZMM@users.noreply.github.com>
2025-09-11 11:17:34 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ HEADERS = {
     "user-agent": "Dart/3.7 (dart:io)",
     "accept-encoding": "gzip",
     "content-type": "application/json",
-    "authorization": os.environ["AUTH_TOKEN"]  # Add AUTH_TOKEN as GitHub secret
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5ZWJjNDUzZS00ZTU5LTRiOGEtYjk0Ny0xMWUwNjUyN2NhZDUiLCJqdGkiOiI0NzdhMzUyYWMwOGVhYWNiMDNiNTNmYzhkY2FmMTQ4YmQ2MWMwNTZhMDcwY2Y2MzJmMWFhYzI5OTQ5MWNjMDE5YzIyZDk3YTQwNGFiOTE2YiIsImlhdCI6MTc1NzQ5OTIxNy40NjQwOTYsIm5iZiI6MTc1NzQ5OTIxNy40NjQxMDQsImV4cCI6MTc4OTAzNTIxNy40NTEyMjEsInN1YiI6IjE3MDkiLCJzY29wZXMiOltdfQ.fHLgsxcD-zvdxWF0tvlW2zZzrQvXTGrcrKkBf1_HkyWYEoKE_Vu13_nVt9BUJQhgeJyN5vj-9rPnWIJmOfjFDaxJODxHuQ3xxHvKi80GBDz7MypXRfMaH0QYcVYGYCnoesL0oOMOuya55NNWF2R5wqlxS7n5CjHHwqbgiV0HQ3qqZn1ifZasLDiS8dufflDDrQoCHj_gHOSg8IHgWQEprrBh-jgH83KdtZkhM7JdXudKF4K_JZycD9u7sCtMRbWqRYCqMiwWD91n2A8dpPgzChsMVB3nLBtCnsI-hRVptq9phunfDK13GtvPiKPTVwnLTWH5TpXgqqTPeYXGdbodbETsIo9AC6f02lWuczXOW2kjKHKXcOxAQnEN4J7unvnuI5Gm_zmbebTzmbOwH2HuanYsEJlmw7cUh52mek2DWBQFXSqg_iKt5DaiCSL8eM20fVDH3PNpgHhadolGgL0msSvdUscSUbLnpU2ekVJZcQwviLM0IkKXih7cPIXgQV2EuUUN09bR4mQvb7H2S3TFXwzs6QUpDv8F10vn0VBsFx9rRqkdT_y7b45XB8XcCYvCCEVkDa6rrx_W343xNQ4HLboVVQkn-wEKMXaNzpoKcdmOrU3sbT169ybt_U-nqAs_YY1MC-o1GknSBeU6nrWUjIb2cIyee8oGu_fkLdTB-gk"  # Add AUTH_TOKEN as GitHub secret
 }
 
 SEARCH_URL = "https://or.smstv.live/api/sub/movie/search"
